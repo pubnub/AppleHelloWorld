036871dd24a47e641d0c4ed145f758d45c8ac610
Jordan Zucker <jordan.zucker@gmail.com>
2015-11-30 15:38:21 -0800

    Basic hello world, untested

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -6,10 +6,12 @@
 //  Copyright © 2015 pubnub. All rights reserved.
 //
 
+#import <PubNub/PubNub.h>
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <PNObjectEventListener>
+@property (nonatomic, weak) IBOutlet UILabel *label;
+@property (nonatomic, strong) PubNub *client;
 @end
 
 @implementation ViewController
@@ -17,11 +19,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.client = [PubNub clientWithConfiguration:[PNConfiguration configurationWithPublishKey:@"demo-36" subscribeKey:@"demo-36"]];
+    [self.client addListener:self];
+    [self.client subscribeToChannels:@[@"helloWorld"] withPresence:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - PNObjectEventListener
+
+- (void)client:(PubNub *)client didReceiveMessage:(PNMessageResult *)message {
+    self.label.text = [NSString stringWithFormat:@"%@", message.data.message];
 }
 
 @end
--- HelloWorld/OSX/ViewController.m
+++ HelloWorld/OSX/ViewController.m
@@ -6,7 +6,13 @@
 //  Copyright © 2015 pubnub. All rights reserved.
 //
 
+#import <PubNub/PubNub.h>
 #import "ViewController.h"
+
+@interface ViewController () <PNObjectEventListener>
+@property (nonatomic, weak) IBOutlet NSTextField *textField;
+@property (nonatomic, strong) PubNub *client;
+@end
 
 @implementation ViewController
 
@@ -14,12 +20,22 @@
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    self.client = [PubNub clientWithConfiguration:[PNConfiguration configurationWithPublishKey:@"demo-36" subscribeKey:@"demo-36"]];
+    [self.client addListener:self];
+    [self.client subscribeToChannels:@[@"helloWorld"] withPresence:YES];
+    
 }
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+#pragma mark - PNObjectEventListener
+
+- (void)client:(PubNub *)client didReceiveMessage:(PNMessageResult *)message {
+    self.textField.stringValue = [NSString stringWithFormat:@"%@", message.data.message];
 }
 
 @end
